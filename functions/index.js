9cb7476d34b1c71731171e9cc163c537bda8bf44
Miki Masumoto <masumotomiki18@gmail.com>
2021-08-04 08:36:22 +0900

    Add inplimentation

--- functions/index.js
+++ functions/index.js
@@ -3,31 +3,15 @@ const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-// Take the text parameter passed to this HTTP endpoint and insert it into 
-// Firestore under the path /messages/:documentId/original
-exports.addMessage = functions.https.onRequest(async (req, res) => {
-    // Grab the text parameter.
-    const original = req.query.text;
-    // Push the new message into Firestore using the Firebase Admin SDK.
-    const writeResult = await admin.firestore().collection('messages').add({ original: original });
-    // Send back a message that we've successfully written the message
-    res.json({ result: `Message with ID: ${writeResult.id} added.` });
+exports.onCreateUser = functions.firestore.document('/users/{docId}').onCreate(async (snap, cxt) => {
+    const docId = cxt.params.docId;
+
+    const originalName = snap.data().name;
+    const lowercaseName = originalName.toUpperCase();
+    functions.logger.log('Set lowercase_name to ', docId, lowercaseName);
+
+    await snap.ref.set({ lowercase_name: lowercaseName, created_at: firebase.getDate() }, { merge: true });
+
+    const eventDocId = snap.ref.parents.collection("logs").add({ event: "createUser", created_at: firebase.getDate(), created_by: docId });
+    functions.logger.log('Add event log ', eventDocId);
 });
-
-// Listens for new messages added to /messages/:documentId/original and creates an
-// uppercase version of the message to /messages/:documentId/uppercase
-exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
-    .onCreate((snap, context) => {
-        // Grab the current value of what was written to Firestore.
-        const original = snap.data().original;
-
-        // Access the parameter `{documentId}` with `context.params`
-        functions.logger.log('Uppercasing', context.params.documentId, original);
-
-        const uppercase = original.toUpperCase();
-
-        // You must return a Promise when performing asynchronous tasks inside a Functions such as
-        // writing to Firestore.
-        // Setting an 'uppercase' field in Firestore document returns a Promise.
-        return snap.ref.set({ uppercase }, { merge: true });
-    });
